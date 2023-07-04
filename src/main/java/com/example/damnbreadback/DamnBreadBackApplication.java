d385ce4db26fe3e97175b78c15f3f11c41b488d0
Gabin Nam <cabin4nam@naver.com>
2023-07-04 15:38:57 +0900

    환경 설정

--- src/main/java/com/example/damnbreadback/DamnBreadBackApplication.java
+++ src/main/java/com/example/damnbreadback/DamnBreadBackApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DamnBreadBackApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DamnBreadBackApplication.class, args);
     }
