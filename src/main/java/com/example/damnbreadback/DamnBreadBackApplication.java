df239f0b2994c67407716389ff794aaf70331980
5lft <zara0140@naver.com>
2023-07-04 15:00:44 +0900

    Initial commit

---
+++ src/main/java/com/example/damnbreadback/DamnBreadBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.damnbreadback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamnBreadBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DamnBreadBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/damnbreadback/DamnBreadBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.damnbreadback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamnBreadBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
