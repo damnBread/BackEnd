48f493b1a4fe55f15dc7a5bbaabebb99ff1224d5
Gabin Nam <cabin4nam@naver.com>
2023-07-27 12:36:32 +0900

    중복확인 수정

--- src/main/java/com/example/damnbreadback/dao/UserDao.java
+++ src/main/java/com/example/damnbreadback/dao/UserDao.java
@@ -77,8 +77,10 @@ public class UserDao {
         ApiFuture<QuerySnapshot> future = db.collection(COLLECTION_NAME).get();
         List<QueryDocumentSnapshot> documents = future.get().getDocuments();
         for (QueryDocumentSnapshot document : documents) {
-            if(document.toObject(User.class).getId().equals(id)){
-                return true;
+            String userId = document.toObject(User.class).getId();
+            if(userId!=null){
+                if(userId.equals(id))
+                    return true;
             }
         }
         return false;
@@ -88,8 +90,10 @@ public class UserDao {
         ApiFuture<QuerySnapshot> future = db.collection(COLLECTION_NAME).get();
         List<QueryDocumentSnapshot> documents = future.get().getDocuments();
         for (QueryDocumentSnapshot document : documents) {
-            if(document.toObject(User.class).getNickname().equals(nickname)){
-                return true;
+            String userNickname = document.toObject(User.class).getNickname();
+            if(userNickname!=null){
+                if(userNickname.equals(nickname))
+                    return true;
             }
         }
         return false;
@@ -99,8 +103,10 @@ public class UserDao {
         ApiFuture<QuerySnapshot> future = db.collection(COLLECTION_NAME).get();
         List<QueryDocumentSnapshot> documents = future.get().getDocuments();
         for (QueryDocumentSnapshot document : documents) {
-            if(document.toObject(User.class).getEmail().equals(email)){
-                return true;
+            String userEmail = document.toObject(User.class).getEmail();
+            if(userEmail!=null){
+                if(userEmail.equals(email))
+                    return true;
             }
         }
         return false;
